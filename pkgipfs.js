a7eb74c1d90b5cc760e7e32ef5a446f450810301
Collin Raddatz <collin.raddatz@gmail.com>
2019-01-31 21:17:40 +0100

    Added binary pkg builds

---
+++ pkgipfs.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+const glob = require('glob');
+const { exec } = require('pkg');
+
+const targetPlatform = process.platform.replace('darwin', 'macos');
+
+const target = `node10-${targetPlatform}-x64`;
+const outDir = path.join('pkg', targetPlatform);
+exec(['package.json', '--target', target, '--out-dir', outDir]).then(() => {
+    let basePath = 'node_modules';
+    let nativeModules = ['keccak', 'leveldown', 'rabin', 'secp256k1', 'ursaNative'];
+
+    nativeModules.forEach((m) => {
+        glob(path.join(basePath, '**', `${m}.node`), function (err, files) {
+            if (err) throw err;
+            if (!files || files.length === 0) throw new Error(`Error finding .node file for module ${m}`);
+            fs.copyFileSync(files[0], path.join(outDir, path.basename(files[0])));
+        });
+    });
+}).catch(e => {
+    console.error(e);
+});
